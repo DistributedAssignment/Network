54f25d379ea3bfbe9d4ee7b182b9ade863d22a39
Luc Murray <lmurray55@qub.ac.uk>
2024-12-03 23:58:23 +0000

    Commit

--- Node.java
+++ Node.java
@@ -518,4 +518,95 @@ public class Node implements Runnable{
 		node.run();
 	}
 
+private class Account {
+
+	private volatile int account_number;
+	private volatile int money;
+	private volatile int overdraft;
+	//The different constructers are useful as account data can exists in a few different forms depending on how it is being processsed
+	public Account(){
+		this.account_number = 0;
+		this.money = 0;
+		this.overdraft = 0;
+	}
+	
+	public Account(int account_number, int money, int overdraft){
+		this.account_number = account_number;
+		this.money = money;
+		this.overdraft = overdraft;
+	}
+	
+	public Account(String data){
+		String[] a = data.split(" ");
+		if (a.length == 3){
+		this.account_number =Integer.parseInt(a[0].trim());
+		this.money = Integer.parseInt(a[1].trim());
+		this.overdraft = Integer.parseInt(a[2].trim());
+		}
+	}
+
+	public Account(String[] a){
+		if (a.length == 3){
+		this.account_number =Integer.parseInt(a[0].trim());
+		this.money = Integer.parseInt(a[1].trim());
+		this.overdraft = Integer.parseInt(a[2].trim());
+		}
+	}
+
+	public int getNumber() {
+		return account_number;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+
+	public int getOverdraft() {
+		return overdraft;
+	}
+	
+	//These are the three methods required to edit an account locally within a node
+	public int withdraw(int withdrawl) {
+	if (withdrawl>=0) {
+		if (money-withdrawl < 0) {
+			if (-1*(money-withdrawl) < overdraft) {
+				money = money-withdrawl;
+				return money;
+			} else {
+				return -1;
+			}
+		} else {
+			money = money-withdrawl;
+			return money;				
+		}
+	} else {
+		return -2;
+	}
+	}
+	
+	public int deposit(int deposit) {
+	if (deposit>=0) {
+		money += deposit;
+		return money;
+	} else {
+		return -1;
+	}
+	}
+	
+	public synchronized void  close() {
+		account_number = -1000;			
+	}
+
+	//Data for an account can be retrieved in the form of a byte[] array or String
+	public byte[] getDataFormat() {
+		String a = Integer.toString(account_number) + " "  +Integer.toString(money) + " "  +Integer.toString(overdraft);
+		byte[] b = a.getBytes();
+		return b;
+	}
+	
+	public String getStringFormat() {
+		String a = Integer.toString(account_number) + " "  +Integer.toString(money) + " "  +Integer.toString(overdraft);
+		return a;
+	}	
+	}
 }
