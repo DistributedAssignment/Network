75ee30c7a254725c2842bfefb8c3657eeeb97295
Luc Murray <lmurray55@qub.ac.uk>
2024-12-03 16:33:11 +0000

    Pray to god

---
+++ Commit.bat
@@ -0,0 +1,6 @@
+git remote remove origin
+git commit -m "Commit"
+git remote add origin https://github.com/DistributedAssignment/Network.git
+git branch -M main
+git fetch -m origin main
+git push -m origin main
